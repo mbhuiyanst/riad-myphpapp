3e197229cbfba71dcbd704666f84271f17f2083a
mbhuiyanst <92376890+mbhuiyanst@users.noreply.github.com>
2024-07-29 15:24:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 echo "Hello, World!";
 echo "Welcome to my Azure Devops Training";
 echo "Hurrah I have succesfully confgiure Github Actions";
+
+echo "Github action is enoug for contunus deployment";
 ?>
